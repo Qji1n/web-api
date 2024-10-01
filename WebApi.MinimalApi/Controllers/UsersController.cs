fa9df5176a9dddb0a352bf7b1bb3978d750a87b9
Qji1n <vit04rom@mail.ru>
2024-10-01 16:55:44 +0500

    Done First Task

--- WebApi.MinimalApi/Controllers/UsersController.cs
+++ WebApi.MinimalApi/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.MinimalApi.Domain;
 using WebApi.MinimalApi.Models;
@@ -8,23 +9,34 @@ namespace WebApi.MinimalApi.Controllers;
 [ApiController]
 public class UsersController : Controller
 {
-    //private readonly IUserRepository _userRepository;
+    private readonly IUserRepository _userRepository;
+    private readonly IMapper _mapper;
     // Чтобы ASP.NET положил что-то в userRepository требуется конфигурация
-    public UsersController(IUserRepository userRepository)
+    public UsersController(IUserRepository userRepository, IMapper mapper)
     {
-        //_userRepository = userRepository;
+        _userRepository = userRepository;
+        _mapper = mapper;
     }
 
     [HttpGet("{userId}")]
+    [Produces("application/json", "application/xml")]
     public ActionResult<UserDto> GetUserById([FromRoute] Guid userId)
     {
-        //var user = _userRepository.FindById(userId);
+        var user = _userRepository.FindById(userId);
 
-        //if (user == null)
-        //{
-        //    return NotFound();
-        //}
-        //return Ok(user);
+        if (user == null)
+        {
+            return NotFound();
+        }
+        var userDto = _mapper.Map<UserDto>(user);
+        //{ 
+        //    //Id = user.Id,
+        //    //Login = user.Login,
+        //    //FullName = $"{user.LastName} {user.FirstName}",
+        //    //GamesPlayed = user.GamesPlayed,
+        //    //CurrentGameId = user.CurrentGameId
+        //};
+        return Ok(userDto);
         throw new NotImplementedException();
     }
 
--- WebApi.MinimalApi/Program.cs
+++ WebApi.MinimalApi/Program.cs
@@ -1,16 +1,30 @@
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Options;
+using NUnit.Framework.Internal;
+using WebApi.MinimalApi.Domain;
+using WebApi.MinimalApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
-//builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
-builder.Services.AddControllers()
-    .ConfigureApiBehaviorOptions(options => {
-        options.SuppressModelStateInvalidFilter = true;
-        options.SuppressMapClientErrors = true;
-    });
-
+builder.Services.AddSingleton<IUserRepository, InMemoryUserRepository>();
+builder.Services.AddControllers(options =>
+{
+    options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+    options.ReturnHttpNotAcceptable = true;
+    options.RespectBrowserAcceptHeader = true;
+})
+.ConfigureApiBehaviorOptions(options => {
+    options.SuppressModelStateInvalidFilter = true;
+    options.SuppressMapClientErrors = true;
+});
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.CreateMap<UserEntity, UserDto>()
+    .ForMember(dest => dest.FullName, opt => opt.MapFrom(src => src.LastName ?? src.FirstName));
+    cfg.CreateMap<UserDto, UserEntity>();
+}, new System.Reflection.Assembly[0]);
 var app = builder.Build();
-
 app.MapControllers();
 
 app.Run();
